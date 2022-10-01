6c130b2d34264024df4eef1085e254a169d3bb13
Fahmida12345 <fahmidayesmin.me@gmail.com>
2022-10-01 18:42:09 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,82 @@
+const myself = {
+       myName: "Fahmida Yeasmin",
+       age   :"22",
+       study :"Diploma In Computer Engineering",
+       studyCenter:"ISTT",
+       metarialStatus: "Marrird",
+    family:{
+        parents:{
+            father:"Md.Alauddin",
+            age   :55,
+            always_say:"Meye to na jeno akta agun Aar gola amr🙂 Atto rag ...Afterall meye to amr e 😂",
+            mother:"Mrs.Nazma Yeasmin",
+            age   :40,
+            always_say:"Amr Brithi moni ja bole ta kore dekhay😊😊",
+        },
+        siblings :{
+            sister:{
+            name :"Farhana Yeasmin",
+            age  :35,
+            myMsg:"World's best sister u r! U dont't know how much i love u ❤️❤️.",
+        },
+    
+            elderBrother:{
+                name :"Mazharul Islam",
+                age  :28,
+                myMsg:"World's best protective brother u r !" ,
+            },
+            youngerBrother:{
+                name :"Monjurul Islam",
+                age  :19,
+                myMsg:"R maris na amare 😥 iktu valo hoi ja" ,
+            },
+    
+        },
+            
+    },
+    my_Legends_Friends:{
+        Rameem:{
+            name :"Mahmood Hossain Rameem",
+            age  : 18,
+           playCharacter:"Everything ok when fahmida say yes! Overall Alavola vai amr  !",
+          always_say:"R a parba tumi❤️...vhoy nai ami✌️😊😊 "
+    },
+    Rasel:{
+        name :"Jahidul Islam Rasel",
+        age  : 21,
+       playCharacter:"Expert of khowadaowa",
+      always_say:" khida lagche vai😥Fahmida cholo Baten re  chuna die😂 "
+},
+     Baten:{
+            name :"Abdul Baten",
+            age  : 21,
+           playCharacter:"Always chuna khay😂But fahmidar vhoy a kichu bole o na ",
+          always_say:"taka nai😥(gujobe kan diben na kau😂) "
+    },
+    Pathan:{
+      name :"Pabel Pathan",
+      age  : 22,
+      playCharacter:"Best photographer in Legend group",
+      always_say:" Ato pora lage naki ? Amni e A+✌️"
+    },
+    Mahabul:{
+            name :"Mahmood Hossain Rameem",
+            age  : 18,
+            playCharacter:"Chupchap...kintu Erin ful hole bapper ta different😜",
+            always_say:"Apu ami kichu komu na🙈 "
+    },
+    Sayma:{
+        name :"Maysha Siddiki",
+            age  : 20,
+            playCharacter:"Sentimental Girl",
+            always_say:"Fahmida tomar moto akta borka banamu😂😂"
+
+
+        }
+
+    },
+
+}
+
+
+console.log(myself.Rameem);
